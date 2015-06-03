b03cba8501d442aeb5b66f110500d166acb7a1d8
abian <abianhernandez@outlook.es>
2015-06-03 18:56:50 +0100

    subiendo de nuevo

--- src/ssh/ScpTransfer.java
+++ src/ssh/ScpTransfer.java
@@ -2,20 +2,39 @@ package ssh;
 
 import java.io.File;
 
+import Connection.ScpUser;
+
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
 
 public class ScpTransfer {
-	private String user;
-	private String host;
+	private String user = "root";
+	private String host = "localhost";
+	private int port = 2200;
+	private String knownHostsPath = "ssh/known_hosts";
+	private String identityKeyPath = "ssh/id_rsa";
 	
-	public void ScpTo(String fileOri, String fileDest){
+	public void ScpTo(String fileOri, String fileDest, boolean timeStamp){
 		try{
 			JSch jsch=new JSch();
 		
-			File knownHostsFile = new File("ssh/known_hosts");
+			File knownHostsFile = new File(knownHostsPath);
 			if (knownHostsFile.exists() && knownHostsFile.isFile()){
-				jsch.setKnownHosts("ssh/known_hosts");
+				jsch.setKnownHosts(knownHostsPath);
+			}
+			
+			File IdentityFile = new File(identityKeyPath);
+			if (IdentityFile.exists() && IdentityFile.isFile()){
+				jsch.addIdentity(identityKeyPath);
+			}
+			
+			Session session = jsch.getSession(user, host, port);
+			session.setUserInfo(new ScpUser());
+			session.connect();
+			
+			if (timeStamp == true){
+				
 			}
 		} catch (JSchException e){
 			e.printStackTrace();
